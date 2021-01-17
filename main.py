d4bff4724267488f101eef7bcd013ced8280f899
Warren Alphonso <warrenalphonso02@gmail.com>
2021-01-16 18:51:59 -0800

    Switched to SendGrid because of Google authentication errors in Heroku

--- main.py
+++ main.py
@@ -1,86 +1,71 @@
-import smtplib
-import ssl
-import csv
 import os
+import sendgrid
+from sendgrid.helpers.mail import *
 from datetime import date
-from email.mime.text import MIMEText
+import csv
 
 
-##
-# SETUP MESSAGE
-##
+sender = From("warrenalphonso.recurring@gmail.com")
+recipient = To("warrenalphonso02@gmail.com")
+
+# Setup messages to send
+messages = {}
 today_date = date.today()
 
 # Sequences
-# Choose post to email by counting days since starting
+# Choose post to send by counting days since starting
 start_date_sequences = date(2020, 10, 31)
-
-title_sequences = ""
-url_sequences = ""
+title_sequence = ""
+url_sequence = ""
 with open("sequences.csv") as f:
     r = csv.reader(f)
     for i, row in enumerate(r):
         if i == (today_date - start_date_sequences).days:
-            title_sequences, url_sequences = row
-
-message_sequences = MIMEText(
-    f"""\
-        <a href="{url_sequences}">{title_sequences}</a>\
+            title_sequence, url_sequence = row
+if title_sequence:
+    messages[f"{title_sequence}"] = \
+        f"""
+        <a href="{url_sequence}">{title_sequence}</a>\
         <br />\
         <a href="https://roamresearch.com/#/app/warrenalphonso">Think in Roam</a>\
         <br />\
         <br />\
         <p>Sent by github.com/warrenalphonso/recurring via Heroku!</p>\
-    """, "html")
-message_sequences["Subject"] = f"{title_sequences}"
+        """
 
-# Toggl Monthly Review
-toggl = today_date.day == 1
-message_toggl = MIMEText(
-    f"""\
+# Toggl Montly Review
+if today_date.day == 1:
+    messages["Time for Toggl Monthly Review"] = \
+        """
         <p>Download the Toggl Detailed Report for the past month in CSV format \
         and see how it went. <b>Push the CSV to GitHub repo.</b></p>
         <br />\
         <p>Sent by github.com/warrenalphonso/recurring via Heroku!</p>\
-    """, "html")
-message_toggl["Subject"] = "Time for Toggl Monthly Review"
+        """
 
 # GoodReads Annual Backup
-goodreads = today_date.month == 1 and today_date.day == 1
-message_goodreads = MIMEText(
-    f"""\
+if today_date.month == 1 and today_date.day == 1:
+    messages["Download GoodReads Data"] = \
+        """
         <p>Download my GoodReads data and save it somewhere.</p>\
         <br />\
         <p>Sent by github.com/warrenalphonso/recurring via Heroku!</p>\
-    """, "html")
-message_goodreads["Subject"] = "Download GoodReads Data"
+        """
 
-##
-# SETUP SMTP CLIENT
-##
-
-port = 465  # GMail requires connecting to port 465 if using SMTP_SLL()
-
-context = ssl.create_default_context()
-
-sender = "warrenalphonso.recurring@gmail.com"
-password = os.environ.get("EMAIL_PASSWORD", None)
-recipient = "warrenalphonso02@gmail.com"
-
-
-with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
-    if password:
-        server.login(sender, password)
-        message_sequences["From"] = sender
-        message_sequences["To"] = recipient
-        server.sendmail(sender, recipient, message_sequences.as_string())
-        if toggl:
-            message_toggl["From"] = sender
-            message_toggl["To"] = recipient
-            server.sendmail(sender, recipient, message_toggl.as_string())
-        if goodreads:
-            message_goodreads["From"] = sender
-            message_goodreads["To"] = recipient
-            server.sendmail(sender, recipient, message_goodreads.as_string())
-    else:
-        print("No password found!")
+# Send
+try:
+    sg = sendgrid.SendGridAPIClient(
+        os.environ.get("SENDGRID_API_KEY", None))
+    for subject, html_content in messages.items():
+        message = Mail(
+            from_email=sender,
+            to_emails=recipient,
+            subject=Subject(subject),
+            html_content=HtmlContent(html_content)
+        )
+        response = sg.send(message=message)
+        if response.status_code != 202:
+            raise Exception(
+                "Response status code wasn't 202. It was: ", response.status_code)
+except Exception as e:
+    print(e)
